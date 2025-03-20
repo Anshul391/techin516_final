28d74edd3d7f98e45bf1b747d93f532bc7e5ede2
Anshul391 <61523975+Anshul391@users.noreply.github.com>
2025-03-19 23:54:28 -0700

    Update setup.py to add kinova arm code

--- turtlebot_move/setup.py
+++ turtlebot_move/setup.py
@@ -20,7 +20,30 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-       		'move_turtlebot = turtlebot_move.move_turtlebot:main',
+       		from setuptools import find_packages, setup
+
+package_name = 'turtlebot_move'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gixstudent',
+    maintainer_email='rkumar23@uw.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+       		'kinova_move = turtlebot_move.kinova_move:main',
+            'gen3lite_pymoveit2 = 516_final.gen3lite_pymoveit2:main',
         ],
     },
 )
